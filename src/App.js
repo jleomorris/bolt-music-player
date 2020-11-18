71b9e2beadf0d58fe97d8daa7512d29ee45c33d1
jleomorris <70540466+jleomorris@users.noreply.github.com>
2020-11-18 14:47:23 +0000

    Add Player and Song components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import Player from "./components/Player";
+import Song from "./components/Song";
+import "./styles/app.scss";
+import data from "./data";
 
 function App() {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+
   return (
     <div className="App">
-      <h1>Music player</h1>
+      <Song currentSong={currentSong} />
+      <Player />
     </div>
   );
 }
