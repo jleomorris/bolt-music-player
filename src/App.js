b81c56b7e4fe168db014780820596e7cae947393
jleomorris <70540466+jleomorris@users.noreply.github.com>
2020-11-26 01:37:58 +0000

    Customize input design

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
   const [songInfo, setSongInfo] = useState({
     currentTime: 0,
     duration: 0,
+    animationPercentage: 0,
   });
   const [isLibraryActive, setIsLibraryActive] = useState(false);
   // Refs
@@ -25,8 +26,16 @@ function App() {
   const timeUpdateHandler = (e) => {
     const current = e.target.currentTime;
     const duration = e.target.duration;
-
-    setSongInfo({ ...songInfo, currentTime: current, duration });
+    const roundedCurrent = Math.round(current);
+    const roundedDuration = Math.round(duration);
+    const animation = Math.round((roundedCurrent / roundedDuration) * 100);
+    console.log(animation);
+    setSongInfo({
+      ...songInfo,
+      currentTime: current,
+      duration,
+      animationPercentage: animation,
+    });
   };
 
   return (
